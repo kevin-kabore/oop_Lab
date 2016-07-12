6abb13225845e1295409e96f35642017064d95d5
Kevin Kabore <kevin.s.kabore@gmail.com>
2016-07-12 15:34:02 -0700

    completes part 5

--- apartment.rb
+++ apartment.rb
@@ -1,4 +1,3 @@
-
 class Apartment
   def initialize unit, num_beds, num_baths
     @unit = unit
@@ -7,12 +6,12 @@ class Apartment
     @tenants = []
   end
 
-  def tenants
-    @tenants << @full_name
-  end
-
   def price
     @price = (1000 * @num_beds) + (500 * @num_baths)
     p "$#{@price}"
+  end
+
+  def studio?
+  	@num_beds == 1 ? true : false
   end
 end
