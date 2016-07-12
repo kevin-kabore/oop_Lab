9ec4874e1cd10fc1f4ccab43c05cce0e4c473362
Kevin Kabore <kevin.s.kabore@gmail.com>
2016-07-12 16:37:30 -0700

    completes part 8

--- apartment.rb
+++ apartment.rb
@@ -14,4 +14,18 @@ class Apartment
   def studio?
   	@num_beds == 1 ? true : false
   end
+
+  def move_in tenant
+  	if full?
+  		p "We're full! #{tenant.full_name} can't move in!"
+  	else
+  		@tenants << tenant
+  	end
+  end
+  def empty?
+  	@tenants.empty?
+  end
+  def full?
+  	@tenants.length == @num_beds
+  end
 end
